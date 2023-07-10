9c49ec2e2d1386fbd0b2dc0300b3910fe6aed572
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 15:14:17 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,41 @@
-﻿namespace Generics
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
 {
-    internal class Program
+    public class ArraySumCalculator
     {
-        static void Main(string[] args)
+        public static T Sum<T>(T[] array)
         {
-            Console.WriteLine("Hello, World!");
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            dynamic sum = default(T);
+            foreach (var element in array)
+            {
+                sum += element;
+            }
+
+            return sum;
+        }
+    }
+
+    class Programs
+    {
+        static void Main()
+        {
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            int intSum = ArraySumCalculator.Sum(intArray);
+            Console.WriteLine("Сума елементів у цілочисельному масиві: " + intSum);
+
+            double[] doubleArray = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+            double doubleSum = ArraySumCalculator.Sum(doubleArray);
+            Console.WriteLine("Сума елементів у масиві дійсних чисел: " + doubleSum);
         }
     }
 }
