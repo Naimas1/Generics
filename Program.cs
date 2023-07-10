b35071ca6c54028cfc1beede9f50d4904c357769
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 15:32:00 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,88 @@
-﻿namespace Generics
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
 {
-    internal class Program
+    public class Queue<T> : IEnumerable<T>
     {
-        static void Main(string[] args)
+        private LinkedList<T> items;
+
+        public Queue()
         {
-            Console.WriteLine("Hello, World!");
+            items = new LinkedList<T>();
+        }
+
+        public void Enqueue(T item)
+        {
+            items.AddLast(item);
+        }
+
+        public T Dequeue()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Черга порожня");
+            }
+
+            T item = items.First.Value;
+            items.RemoveFirst();
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Черга порожня");
+            }
+
+            return items.First.Value;
+        }
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    class Programu
+    {
+        static void Main()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("один");
+            queue.Enqueue("два");
+            queue.Enqueue("три");
+
+            Console.WriteLine("Кількість елементів у черзі: " + queue.Count);
+
+            string firstItem = queue.Dequeue();
+            Console.WriteLine("Перший елемент у черзі: " + firstItem);
+
+            string peekedItem = queue.Peek();
+            Console.WriteLine("Елемент, який дивимося в черзі: " + peekedItem);
+
+            Console.WriteLine("Кількість елементів у черзі після видалення: " + queue.Count);
+
+            Console.WriteLine("Елементи у черзі:");
+            foreach (string item in queue)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
