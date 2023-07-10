9f768cb8ab33a6dba5304aee3c1332f46b0a89df
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 14:10:54 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Generics
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
 {
-    internal class Program
+    class Programn
     {
-        static void Main(string[] args)
+        static void Swap<T>(ref T a, ref T b)
         {
-            Console.WriteLine("Hello, World!");
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        static void Main()
+        {
+            int num1 = 10;
+            int num2 = 20;
+
+            Console.WriteLine($"До обміну: num1 = {num1}, num2 = {num2}");
+
+            Swap(ref num1, ref num2);
+
+            Console.WriteLine($"Після обміну: num1 = {num1}, num2 = {num2}");
+
+            string str1 = "Hello";
+            string str2 = "World";
+
+            Console.WriteLine($"До обміну: str1 = {str1}, str2 = {str2}");
+
+            Swap(ref str1, ref str2);
+
+            Console.WriteLine($"Після обміну: str1 = {str1}, str2 = {str2}");
         }
     }
 }
