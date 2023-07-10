41e092031483f56ea1d45c0fe05afcef731d5675
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-10 15:03:44 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Generics
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
 {
-    internal class Program
+    public class MinimumCalculator
     {
-        static void Main(string[] args)
+        public static T FindMinimum<T>(T first, T second, T third) where T : IComparable<T>
         {
-            Console.WriteLine("Hello, World!");
+            if (first.CompareTo(second) <= 0 && first.CompareTo(third) <= 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) <= 0 && second.CompareTo(third) <= 0)
+            {
+                return second;
+            }
+            else
+            {
+                return third;
+            }
+        }
+    }
+
+    class Programe
+    {
+        static void Main()
+        {
+            int minInt = MinimumCalculator.FindMinimum(10, 5, 8);
+            Console.WriteLine("Мінімум з трьох цілих чисел: " + minInt);
+
+            double minDouble = MinimumCalculator.FindMinimum(3.14, 2.71, 2.99);
+            Console.WriteLine("Мінімум з трьох дійсних чисел: " + minDouble);
+
+            string minString = MinimumCalculator.FindMinimum("apple", "banana", "cherry");
+            Console.WriteLine("Мінімум з трьох рядків: " + minString);
         }
     }
 }
